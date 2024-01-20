bb29bd4ecb1b1cf9d9269f9290bf58e575ccf257
Daniel Ruiz <danielruimar@gmail.com>
2024-01-20 10:20:12 +0100

    Initial commit

--- src/domain/entities/user-entity.ts
+++ src/domain/entities/user-entity.ts
@@ -1,61 +1,3 @@
-/**
- * @swagger
- * components:
- *   schemas:
- *     User:
- *       type: object
- *       required:
- *         - firstName
- *         - lastName
- *         - email
- *         - password
- *         - rol
- *       properties:
- *         _id:
- *           type: string
- *           description: ID del usuario.
- *         firstName:
- *           type: string
- *           description: Nombre del usuario.
- *           minLength: 3
- *           maxLength: 22
- *         lastName:
- *           type: string
- *           description: Apellido del usuario.
- *           minLength: 3
- *           maxLength: 22
- *         email:
- *           type: string
- *           description: Email del usuario.
- *           format: email
- *         password:
- *           type: string
- *           description: Contraseña del usuario.
- *           minLength: 8
- *         rol:
- *           type: string
- *           description: Rol del usuario.
- *           enum:
- *             - PLAYER
- *             - MANAGER
- *             - ADMIN
- *         team:
- *           type: string
- *           description: ID del equipo al que pertenece el usuario.
- *         image:
- *           type: string
- *           description: URL de la imagen del usuario.
- *       example:
- *         _id: 60c84e71ebeb8f001ff60999
- *         firstName: John
- *         lastName: Doe
- *         email: john.doe@example.com
- *         password: [hidden]
- *         rol: PLAYER
- *         team: 60c84e71ebeb8f001ff60998
- *         image: https://example.com/johndoe.png
- */
-
 import mongoose, { Document } from "mongoose";
 
 import validator from "validator";
@@ -87,14 +29,14 @@ const userSchema = new Schema<IUserCreate>(
       trim: true,
       minLength: [3, "Al menos tres letras para el nombre"],
       maxLength: [22, "Nombre demasiado largo, máximo de 22 caracteres"],
-      required: true
+      required: true,
     },
     lastName: {
       type: String,
       trim: true,
       minLength: [3, "Al menos tres letras para el nombre"],
       maxLength: [22, "Nombre demasiado largo, máximo de 22 caracteres"],
-      required: true
+      required: true,
     },
     email: {
       type: String,
@@ -104,32 +46,31 @@ const userSchema = new Schema<IUserCreate>(
         validator: (text: string) => validator.isEmail(text),
         message: "Email incorrecto",
       },
-      required: true
+      required: true,
     },
     password: {
       type: String,
       trim: true,
       minLength: [8, "La contraseña debe tener al menos 8 caracteres"],
       select: false, // Indica que no lo deseamos mostrar cuando se realicen las peticiones.
-      required: true
+      required: true,
     },
     rol: {
       type: String,
       enum: ROL,
       default: ROL.PLAYER,
-      required: true
+      required: true,
     },
     team: {
       type: Schema.Types.ObjectId,
       ref: Team,
-      required: false
+      required: false,
     },
     image: {
       type: String,
       trim: true,
-      required: false
+      required: false,
     },
-
   },
   { timestamps: true } // Cada vez que se modifique un documento refleja la hora y fecha de modificación
 );
@@ -150,6 +91,6 @@ userSchema.pre("save", async function (next) {
   }
 });
 // Creamos tipos para usuarios
-export type IUser = IUserCreate & Document
+export type IUser = IUserCreate & Document;
 // Creamos un modelo para que siempre que creamos un user valide contra el Schema que hemos creado para ver si es valido.
 export const User = mongoose.model<IUserCreate>("User", userSchema);
