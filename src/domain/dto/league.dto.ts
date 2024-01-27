874ea8683826e239236478c46a0247c7387e16b7
Daniel Ruiz <danielruimar@gmail.com>
2024-01-27 09:18:02 +0100

    Revert "f"
    This reverts commit 0e05b6f9db131da49031ad9067700597e51a0d2d.

--- src/domain/dto/league.dto.ts
+++ src/domain/dto/league.dto.ts
@@ -1,10 +1,50 @@
 import { CustomError } from "../../server/checkError.middleware";
-
 import { IMatchCreate } from "../entities/match-entity";
+<<<<<<< HEAD
 import { ITeam } from "../entities/team-entity";
+=======
+import { ITeam, ITeamsStatistics } from "../entities/team-entity";
+import { IUser } from "../entities/user-entity";
+>>>>>>> parent of 0e05b6f (f)
 
-import { convertDateStringToDate } from "../../utils/convertDateStringToDate";
+const generateRandomGoalForIdplayers = (players: IUser[], minId: number, maxId: number): IUser[] => {
+  const goalIds: IUser[] = [];
+  const numGoals = Math.floor(Math.random() * 4);
 
+  for (let i = 0; i < numGoals; i++) {
+    const playerId = Math.floor(Math.random() * (maxId - minId + 1)) + minId;
+    goalIds.push(players[playerId].id);
+  }
+
+  return goalIds;
+};
+
+const convertDateStringToDate = (dateString: string): Date => {
+  const [day, month, year] = dateString.split("/").map(Number);
+  const fullYear = year < 100 ? year + 2000 : year;
+
+  return new Date(fullYear, month - 1, day);
+};
+
+const checkParsedStartDateForCreateLeague = (startDateString: string): Date => {
+  const actualDate: Date = new Date();
+
+  if (!startDateString) {
+    throw new CustomError("Tiene que introducir una fecha de inicio con formato:'21/5/4' para realizar esta operación", 404);
+  }
+
+  const startDate = leagueDto.convertDateStringToDate(startDateString);
+
+  if (actualDate > startDate) {
+    throw new CustomError("La fecha tiene que ser posterior a la actual", 404);
+  }
+
+  return startDate;
+};
+
+<<<<<<< HEAD
+const checkAreTeamsNumberCorrectPerCreateLeagueAndShuffleIteamArray = (teamList: ITeam[]): ITeam[] => {
+=======
 const checkAreTeamsCorrectToCreateLeague = (teams: ITeam[]): void => {
   if (!teams || teams.length === 0) {
     throw new CustomError("No hay equipos en la BBDD.", 400);
@@ -15,23 +55,8 @@ const checkAreTeamsCorrectToCreateLeague = (teams: ITeam[]): void => {
   }
 };
 
-const checkParsedStartDateForCreateLeague = (startDateString: string): Date => {
-  const actualDate: Date = new Date();
-
-  if (!startDateString) {
-    throw new CustomError("Tiene que introducir una fecha de inicio con formato:'21/5/4' para realizar esta operación", 404);
-  }
-
-  const startDate = convertDateStringToDate(startDateString);
-
-  if (actualDate > startDate) {
-    throw new CustomError("La fecha tiene que ser posterior a la actual", 404);
-  }
-
-  return startDate;
-};
-
-const checkAreTeamsNumberCorrectPerCreateLeagueAndShuffleIteamArray = (teamList: ITeam[]): ITeam[] => {
+const checkTeamsNumberIsCorrectPerCreateLeagueAndShuffleIteamArray = (teamList: ITeam[]): ITeam[] => {
+>>>>>>> parent of 0e05b6f (f)
   const newTeamList = [...teamList];
 
   checkAreTeamsCorrectToCreateLeague(newTeamList);
@@ -71,6 +96,89 @@ const showDataLeague = (matches: IMatchCreate[], numCheckedTeams: number, numRou
   `);
 };
 
+<<<<<<< HEAD
+=======
+const initializeTeam = (teamId: string, teamName: string, teamInitials: string, teamImage: string): ITeamsStatistics => ({
+  id: teamId,
+  name: teamName,
+  initials: teamInitials,
+  image: teamImage,
+  matchesPlayed: 0,
+  matchesWon: 0,
+  matchesLost: 0,
+  matchesDrawn: 0,
+  points: 0,
+  goalsFor: 0,
+  goalsAgainst: 0,
+  position: 0,
+});
+
+const updateTeamsStatisticsPerMatch = (match: IMatchCreate, localTeamStatics: ITeamsStatistics, visitorTeamStatics: ITeamsStatistics): void => {
+  if (match.played && match.goalsLocal && match.goalsVisitor) {
+    const localGoals = match.goalsLocal.length || 0;
+    const visitorGoals = match.goalsVisitor.length || 0;
+
+    if (localGoals > visitorGoals) {
+      leagueDto.updateTeamStatistics(localTeamStatics, localGoals, visitorGoals, 3, "win");
+      leagueDto.updateTeamStatistics(visitorTeamStatics, visitorGoals, localGoals, 0, "loss");
+    } else if (localGoals < visitorGoals) {
+      leagueDto.updateTeamStatistics(visitorTeamStatics, visitorGoals, localGoals, 3, "win");
+      leagueDto.updateTeamStatistics(localTeamStatics, localGoals, visitorGoals, 0, "loss");
+    } else {
+      leagueDto.updateTeamStatistics(localTeamStatics, localGoals, visitorGoals, 1, "draw");
+      leagueDto.updateTeamStatistics(visitorTeamStatics, visitorGoals, localGoals, 1, "draw");
+    }
+  }
+};
+
+const updateTeamStatistics = (teamStatics: ITeamsStatistics, goalsFor: number, goalsAgainst: number, points: number, matchResult: string): void => {
+  teamStatics.goalsFor += goalsFor;
+  teamStatics.goalsAgainst += goalsAgainst;
+  teamStatics.matchesPlayed++;
+
+  switch (matchResult) {
+    case "win":
+      teamStatics.points += points;
+      teamStatics.matchesWon++;
+      break;
+    case "loss":
+      teamStatics.matchesLost++;
+      break;
+    case "draw":
+      teamStatics.points++;
+      teamStatics.matchesDrawn++;
+      break;
+  }
+};
+
+const calculateLeagueStatisticsPerTeam = (matches: IMatchCreate[]): ITeamsStatistics[] => {
+  const teams: Record<string, ITeamsStatistics> = {};
+
+  for (const match of matches) {
+    const { _id: localTeamId, name: localTeamName, initials: localTeamInitials, image: localTeamImage } = match.localTeam;
+    const { _id: visitorTeamId, name: visitorTeamName, initials: visitorTeamInitials, image: visitorTeamImage } = match.visitorTeam;
+
+    teams[localTeamId] = teams[localTeamId] || leagueDto.initializeTeam(localTeamId, localTeamName, localTeamInitials, localTeamImage as string);
+    teams[visitorTeamId] = teams[visitorTeamId] || leagueDto.initializeTeam(visitorTeamId, visitorTeamName, visitorTeamInitials, visitorTeamImage as string);
+
+    const localTeam = teams[localTeamId];
+    const visitorTeam = teams[visitorTeamId];
+
+    leagueDto.updateTeamsStatisticsPerMatch(match, localTeam, visitorTeam);
+  }
+
+  const teamStatistics = Object.values(teams);
+
+  teamStatistics.sort((a, b) => b.points - a.points);
+
+  teamStatistics.forEach((team, index) => {
+    team.position = index + 1;
+  });
+
+  return teamStatistics;
+};
+
+>>>>>>> parent of 0e05b6f (f)
 const generateMatchesPerLeague = (checkedTeams: ITeam[], startDate: Date): IMatchCreate[] => {
   const matchesInLeague: IMatchCreate[] = [];
 
@@ -147,9 +255,21 @@ const generateMatch = (teams: ITeam[], home: number, away: number, startDate: Da
 
 export const leagueDto = {
   showDataLeague,
+<<<<<<< HEAD
   checkParsedStartDateForCreateLeague,
   checkAreTeamsCorrectToCreateLeague,
   checkAreTeamsNumberCorrectPerCreateLeagueAndShuffleIteamArray,
+=======
+  validateAndParsedStartDateForCreateLeague,
+  convertDateStringToDate,
+  checkAreTeamsCorrectToCreateLeague,
+  checkTeamsNumberIsCorrectPerCreateLeagueAndShuffleIteamArray,
+  initializeTeam,
+  updateTeamStatistics,
+  updateTeamsStatisticsPerMatch,
+  calculateLeagueStatisticsPerTeam,
+  generateRandomGoalForIdplayers,
+>>>>>>> parent of 0e05b6f (f)
   generateMatch,
   generateMatchesPerRound,
   generateMatchesPerLap,
